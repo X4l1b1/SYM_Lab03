741bc0a6035f9edab7c2dcc3fe57a42d46f4ef81
Marie <mariepascalemdjo2@gmail.com>
2017-12-15 15:59:44 +0100

    just copiyed the code given

--- Labo3-Template/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/NFCActivity.java
+++ Labo3-Template/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/NFCActivity.java
@@ -1,8 +1,12 @@
 package ch.heigvd.iict.sym.a3dcompassapp;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
@@ -31,4 +35,50 @@ public class NFCActivity extends AppCompatActivity implements {
 
 
     }
+
+    @Override
+    protected void onResume() { //enable sensors
+        super.onResume();
+        setupForegroundDispatch();
+
+    }
+
+    @Override
+    protected void onPause() {//desable sensors
+        super.onPause();
+        stopForegroundDispatch();
+    }
+
+    // called in onResume()
+    private void setupForegroundDispatch() {
+
+        if(mNfcAdapter == null)
+            return;
+
+        final Intent intent = new Intent(this.getApplicationContext(),   this.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent =  PendingIntent.getActivity(this.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType("text/plain");
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            Log.e(TAG, "MalformedMimeTypeException", e);
+        }
+        mNfcAdapter.enableForegroundDispatch(this, pendingIntent, filters, techList);
+    }
+
+    // called in onPause()
+    private void stopForegroundDispatch() {
+        if(mNfcAdapter != null)
+            mNfcAdapter.disableForegroundDispatch(this);
+    }
+
 }
