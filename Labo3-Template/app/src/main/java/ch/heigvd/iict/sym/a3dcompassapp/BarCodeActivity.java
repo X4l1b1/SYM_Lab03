2323df678fe443a8083128d0a49f65e55abb14f4
psrochat <pierre.samuel.rochat@gmail.com>
2017-12-15 14:08:51 +0100

    Code barre

--- Labo3-Template/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/BarCodeActivity.java
+++ Labo3-Template/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/BarCodeActivity.java
@@ -1,7 +1,12 @@
 package ch.heigvd.iict.sym.a3dcompassapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 /**
  * Created by pierre-samuelrochat on 08.12.17.
@@ -15,7 +20,24 @@ public class BarCodeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bar_code);
+        new IntentIntegrator(this).initiateScan();
 
     }
+
+    // Get the results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
 }
