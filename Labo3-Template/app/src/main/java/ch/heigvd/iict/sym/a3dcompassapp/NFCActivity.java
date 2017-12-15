9a385e882d52257f3906322c02605cd566dbd7a2
Marie <mariepascalemdjo2@gmail.com>
2017-12-15 15:48:15 +0100

    NFC began

--- Labo3-Template/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/NFCActivity.java
+++ Labo3-Template/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/NFCActivity.java
@@ -1,20 +1,34 @@
 package ch.heigvd.iict.sym.a3dcompassapp;
 
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /**
  * Created by pierre-samuelrochat on 08.12.17.
  */
 
-public class NFCActivity extends AppCompatActivity {
+public class NFCActivity extends AppCompatActivity implements {
     // For logging purposes
     private static final String TAG = BarCodeActivity.class.getSimpleName();
+    NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
+
+        // Check for available NFC Adapter
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        // Register callback
+        mNfcAdapter.setNdefPushMessageCallback(this, this);
+
 
     }
 }
